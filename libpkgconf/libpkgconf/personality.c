2949c0799016467b1dc4d32c43c4b58ab6c5e04c
Boris Kolpackov <boris@codesynthesis.com>
2022-06-30 09:08:44 +0200

    Switch to original ISC license
    The forked license appears to be a custom version of ISC with a
    simplified warranty disclaimer. By reading through the original
    and this version of the wording, one can reasonably conclude that
    the original disclaimer covers everything that the simplified
    version does and thus switching to the original should not be a
    problem.
    
    Speaking of licenses, there are two source files that are covered
    by other licenses:
    
    bsdstubs.c   -- OpenBSD ISC (without "and/or")
    win-dirent.h -- MIT

--- libpkgconf/libpkgconf/personality.c
+++ libpkgconf/libpkgconf/personality.c
@@ -2,15 +2,21 @@
  * personality.c
  * libpkgconf cross-compile personality database
  *
- * Copyright (c) 2018 pkgconf authors (see AUTHORS).
+ * ISC License
+ *
+ * Copyright (c) 2011-2018 pkgconf authors (see AUTHORS file).
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * This software is provided 'as is' and without any warranty, express or
- * implied.  In no event shall the authors be liable for any damages arising
- * from the use of this software.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <libpkgconf/config.h>
